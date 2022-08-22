03297e4db12ffb3f59a29d41da8f81dc134ecfc2
salawu <a.salawu@outlook.com>
2022-08-22 13:14:24 +0000

    desktop frequently asked section

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+//changes navbar style on scroll
 window.addEventListener("scroll", () => {
     document.querySelector('nav').classList.toggle('window-scroll',window.scrollY > 60)
+})
+
+
+//show or hide faq answers
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach((faq) => {
+    faq.addEventListener('click',() => {
+        faq.classList.toggle('open')
+
+        // change
+        const icon = faq.querySelector('.faq__icon i');
+        if (icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-minus'
+        } else {
+            icon.className = 'uil uil-plus'
+        }
+    })
 })
